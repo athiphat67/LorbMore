3f4bf80245144dd9e8c9a005432a2112fa86db08
athiphat67 <athiphat.sun@dome.tu.ac.th>
2025-11-04 22:12:04 +0700

    edit urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [
-    path('', views.about_page_view, name='about'),
+    path('about/', views.about_page_view, name='about'),
+    path('', views.home_page_view, name='home'),
 ]
