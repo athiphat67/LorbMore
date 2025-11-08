2a067f6c81460fdfec282ac58986b63aff9c98e8
lalita <6710615243@gmail.com>
2025-11-09 02:11:37 +0700

    urls_add

--- my_project/urls.py
+++ my_project/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     
 ]
 
+# สำหรับเสิร์ฟไฟล์ media ตอน DEBUG=True (ใน local เท่านั้น)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # pragma: no cover
