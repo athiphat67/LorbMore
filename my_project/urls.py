4b587089314f08ec63984937027866a021da809e
Lalita Thatsananunchai <6710615243@student.tu.ac.th>
2025-11-09 05:11:29 +0700

    delete comment

--- my_project/urls.py
+++ my_project/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
 
 # สำหรับเสิร์ฟไฟล์ media ตอน DEBUG=True (ใน local เท่านั้น)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # pragma: no cover
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
