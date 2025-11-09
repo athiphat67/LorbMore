b9e7fe7fc529997f20aaae238550ac576f1e5a6a
athiphat67 <athiphat.sun@dome.tu.ac.th>
2025-11-09 15:32:52 +0700

    Update urls.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 ]
 
 # สำหรับเสิร์ฟไฟล์ media ตอน DEBUG=True (ใน local เท่านั้น)
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
