5c6dc96ee652a8b7b0fbd501892dc8dbddc4cbaa
athiphat67 <athiphat.sun@dome.tu.ac.th>
2025-11-10 15:03:17 +0700

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('hiring/', views.hiring_page_view, name='hiring'),
     path('rental/', views.rental_page_view, name='rental'),
     path('post/<int:post_id>/', views.detail_post_view, name='detail_post'), 
+    path('create/', views.createpost, name='createposts'), 
+    path('create/hiring/', views.create_hiring_view, name='create_hiring'),
+    path('create/rental/', views.create_rental_view, name='create_rental'),
 ]
