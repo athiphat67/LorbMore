7bf7448afd6d6961073c9ad7fdad80b8a09b1e49
athiphat67 <athiphat.sun@dome.tu.ac.th>
2025-12-10 13:01:44 +0700

    update user can upload profile

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Profile
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'displayName', 'studentId', 'show_image')
+    list_display_links = ('user', 'displayName')
+    readonly_fields = ('show_image',)
+
+    def show_image(self, obj):
+        if obj.profile_image:
+            # แสดงรูปขนาดความสูงไม่เกิน 100px
+            return format_html('<img src="{}" style="max-height: 100px; border-radius: 5px;" />', obj.profile_image.url)
+        return "-"
+    
+    show_image.short_description = 'รูปโปรไฟล์ปัจจุบัน' 
+
+admin.site.register(Profile, ProfileAdmin)
--- users/views.py
+++ users/views.py
@@ -4,35 +4,39 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import UserUpdateForm, ProfileUpdateForm
 
+
 # ฟังก์ชันสำหรับ "ดูโปรไฟล์" (ใครเข้าก็ได้ หรือจะบังคับ login ก็ได้)
 def profile_detail_view(request, username):
     # ค้นหา User จาก username ที่ส่งมาใน URL
     profile_user = get_object_or_404(User, username=username)
-    
+
     context = {
-        'profile_user': profile_user,
+        "profile_user": profile_user,
     }
-    return render(request, 'users/profile_detail.html', context)
+    return render(request, "users/profile_detail.html", context)
+
 
 # ฟังก์ชันสำหรับ "แก้ไขโปรไฟล์" (ต้อง Login เท่านั้น)
 @login_required
 def profile_edit_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            return redirect('profile_detail', username=request.user.username) 
+            return redirect("profile_detail", username=request.user.username)
+        else:
+            # เพิ่มบรรทัดนี้เพื่อเช็ค Error ใน Terminal
+            print("User Form Errors:", u_form.errors)
+            print("Profile Form Errors:", p_form.errors)
 
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
+    context = {"u_form": u_form, "p_form": p_form}
 
-    return render(request, 'users/profile_edit.html', context)
+    return render(request, "users/profile_edit.html", context)
